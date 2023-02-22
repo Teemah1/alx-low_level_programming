82bd3cd0138ac54e51bb0a91c09bed163d507a47
Teemah1 <Bakarefatimoh09@gmail.com>
2023-02-21 21:29:40 -0800

    Upper case

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha -  checks if char is upper case
+ * @c: integer
+ * Return: 1(success), 0(fail)
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+}
