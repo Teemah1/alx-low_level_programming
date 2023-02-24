76d74c626ad3567b1ea2137d9fef2111793f7911
Teemah1 <Bakarefatimoh09@gmail.com>
2023-02-24 00:03:28 -0800

    Printing numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print nums from 0-9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+}
