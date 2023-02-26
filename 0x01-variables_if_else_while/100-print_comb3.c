c33f5c520e32d1ab5239660febbf2a19ca758578
Teemah1 <Bakarefatimoh09@gmail.com>
2023-02-26 01:31:15 -0800

    Printing 2 combinations of numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	int i, k;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (k = 0; k < 10; k++)
+		{
+			putchar((i % 10) + '0');
+			putchar((k % 10) + '0');
+
+			if (i < 8 || k < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
